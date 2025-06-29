eeb087e01d90d3a702a49565df48c6daa11745e8
wptasmina <wptasmina@gmail.com>
2025-06-29 16:20:33 +0600

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { Navigation } from "@/components/navigation";
-
+import { Hero } from "@/components/hero";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center items-center bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
-     <Navigation />
+     <div className="mt-20">
+      <Hero />
+      {/* <About />
+      <Skills />
+      <Projects />
+      <Contact /> */}
+     </div>
     </main>
   );
 }
