a908247ff7ab0318b16fab0debdfc28c130e5365
wptasmina <wptasmina@gmail.com>
2025-06-30 01:24:20 +0600

    fix: error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/about/about-section";
 import Contact from "@/components/contact/Contact";
 import FeatureSection from "@/components/Features/FeatureSection";
 import { Hero } from "@/components/hero";
-import TestimonialSlider from "@/components/Testimonial";
+// import TestimonialSlider from "@/components/Testimonial";
 import WorkProcess from "@/components/work-process";
 
 
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
      <div className="mt-20">
-      <TestimonialSlider />;
+      {/* <TestimonialSlider />; */}
       <Hero />
       <FeatureSection />
       <About />
