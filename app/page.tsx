06b592a7fe07f9fce7b91595f9d9cf19d8bf3f7f
wptasmina <wptasmina@gmail.com>
2025-06-29 14:05:15 +0600

    add navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-
-
+import { Navigation } from "@/components/navigation";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center items-center bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
-      <h2 className="text-6xl">Hello world</h2>
+     <Navigation />
     </main>
   );
 }
